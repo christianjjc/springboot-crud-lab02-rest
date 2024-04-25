7dfd22f90e86e51a8494c874953c003a1bda58e8
Christian Jiménez <christianjjc@gmail.com>
2024-04-24 20:01:11 -0500

    initial commit - project finished v01

---
+++ src/main/java/edu/pe/cibertec/Lab02RestApplication.java
@@ -0,0 +1,13 @@
+package edu.pe.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab02RestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab02RestApplication.class, args);
+    }
+
+}
---
+++ src/main/java/edu/pe/cibertec/controller/CursoController.java
@@ -0,0 +1,51 @@
+package edu.pe.cibertec.controller;
+
+import edu.pe.cibertec.model.CursoEntity;
+import edu.pe.cibertec.service.CursoService;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class CursoController {
+
+    @Getter
+    @Setter
+    @Autowired
+    private CursoService cursoService;
+
+    @GetMapping("cursos")
+    public List<CursoEntity> listarTodos(){
+        return cursoService.listarTodos();
+    }
+
+    @GetMapping(value = "cursos/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public CursoEntity obtenerCurso(@PathVariable (value="id") Integer id){
+        return getCursoService().obtenerCurso(id);
+    }
+
+    @DeleteMapping(value = "cursos/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public boolean eliminarCurso(@PathVariable (value="id") Integer id){
+        return getCursoService().deleteCurso(id);
+    }
+
+    @PostMapping(value = "cursos",produces = MediaType.APPLICATION_JSON_VALUE)
+    public CursoEntity guardarCurso(@RequestBody CursoEntity curso){
+        return getCursoService().saveUpdateCurso(curso);
+    }
+
+    @PutMapping(value = "cursos/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public CursoEntity actualizarCurso(@RequestBody CursoEntity curso,@PathVariable (value="id") Integer id){
+        CursoEntity cursActualizar = new CursoEntity();
+        curso.setId(id);
+        BeanUtils.copyProperties(curso, cursActualizar);
+        return getCursoService().saveUpdateCurso(cursActualizar);
+    }
+
+
+}
